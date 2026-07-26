a4d07c8bc200381e11e25b091ca944215ad5b1e2
Pallavi Pandey <pallavipandey843@gmail.com>
2026-07-27 01:40:20 +0530

    Setup FastAPI backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,12 @@
+from fastapi import FastAPI
 
+app = FastAPI(
+    title="Crop Yield Prediction & Agricultural Productivity Forecasting System",
+    version="1.0.0"
+)
+
+@app.get("/")
+def home():
+    return {
+        "message": "Welcome to Crop Yield Prediction & Agricultural Productivity Forecasting System"
+    }
