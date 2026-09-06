b8a4db6204dfe0aa72325a9dc0674365df8c213b
Pallavi Pandey <pallavipandey843@gmail.com>
2026-09-06 07:07:13 +0530

    Allow deployed frontend in CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -87,8 +87,8 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:8080",
         "http://127.0.0.1:8080",
+        "https://yieldsense-frontend-1w3d.onrender.com",
     ],
-
     allow_credentials=True,
 
     allow_methods=["*"],
